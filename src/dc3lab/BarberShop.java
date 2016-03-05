577bc6f52e5358bb06b11a4ca6f1c0142762d833
proggeo <mashtaliryurko@gmail.com>
2016-03-05 23:55:11 +0200

    dc3lab task 2 update(added synchronized)

--- src/dc3lab/BarberShop.java
+++ src/dc3lab/BarberShop.java
@@ -6,11 +6,10 @@ import java.util.ArrayList;
  * Created by Proggeo on 3/5/2016.
  */
 public class BarberShop {
-    static ArrayList visitors = new ArrayList();
-    static Barberer master;
+    static ArrayList<Visitor> visitors = new ArrayList();
+    final static Barberer master = new Barberer();
 
     public static void main(String[] args) {
-        master = new Barberer();
         master.start();
 
     }
@@ -23,15 +22,27 @@ class Barberer extends Thread{
 
     @Override
     public void run() {
-        if(BarberShop.visitors.isEmpty()){
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while(true){
+            if(BarberShop.visitors.isEmpty()){
+                try {
+                    synchronized (this) {
+                        this.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        else{
-
+            else{
+                Visitor nextVisitor = BarberShop.visitors.remove(0);
+                try { // barbering
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (nextVisitor) {
+                    nextVisitor.notify();
+                }
+            }
         }
     }
 }
@@ -39,6 +50,17 @@ class Barberer extends Thread{
 class Visitor extends Thread {
     @Override
     public void run() {
-        if(BarberShop.master.)
+        if(BarberShop.master.getState()==State.WAITING){
+            synchronized (BarberShop.master){
+                BarberShop.master.notify();
+            }
+        }
+        synchronized (this){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
